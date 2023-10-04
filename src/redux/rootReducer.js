965d1f783e5e909dd4e095d6e464e144605c1e1a
hlushak <75501308+MarynaHl@users.noreply.github.com>
2023-10-04 22:56:30 +0100

    upd

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,29 +1,29 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import { combineReducers } from '@reduxjs/toolkit';
+// import { persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 
-import authReducer from './auth/authSlice';
-import petsReducer from './pets/petsSlice';
-import noticesReducer from './notices/noticesSlice';
-import filterReducer from './filter/filterSlice';
-import modalReducer from './modal/modalSlice';
-import requestParamsReducer from './filter/requestParamsSlice';
+// import authReducer from './auth/authSlice';
+// import petsReducer from './pets/petsSlice';
+// import noticesReducer from './notices/noticesSlice';
+// import filterReducer from './filter/filterSlice';
+// import modalReducer from './modal/modalSlice';
+// import requestParamsReducer from './filter/requestParamsSlice';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['token'],
-};
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   whitelist: ['token'],
+// };
 
-const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+// const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
-const rootReducer = combineReducers({
-  auth: persistedAuthReducer,
-  pets: petsReducer,
-  notices: noticesReducer,
-  filter: filterReducer,
-  modal: modalReducer,
-  requestParams: requestParamsReducer,
-});
+// const rootReducer = combineReducers({
+//   auth: persistedAuthReducer,
+//   pets: petsReducer,
+//   notices: noticesReducer,
+//   filter: filterReducer,
+//   modal: modalReducer,
+//   requestParams: requestParamsReducer,
+// });
 
-export default rootReducer;
+// export default rootReducer;
